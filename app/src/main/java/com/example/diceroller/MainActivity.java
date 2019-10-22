e7409d938afb2b62085d1b562e78567fc808a089
Tofi <1812703@brunel.ac.uk>
2019-10-22 18:29:48 +0100

    Added small extra features (Level 2 of Part 1)

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,10 +20,21 @@ public class MainActivity extends AppCompatActivity {
     public void on_button_click(View view) {
 
         TextView tv = this.findViewById(R.id.numberTextView) ;
+        TextView en = (TextView) this.findViewById(R.id.EnterNumber);
+        TextView ms = this.findViewById(R.id.Message);
 
         Random r = new Random();
         int number = r.nextInt (6);
 
+
         tv.setText(Integer.toString(number));
+
+
+        int Number = Integer.valueOf(tv.getText().toString());
+        int UserNumber = Integer.valueOf(en.getText().toString());
+
+        if (UserNumber == Number) {
+            Toast.makeText(getApplicationContext(), "You guessed the same number, congratulations!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
