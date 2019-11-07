926e0500c431da902a107119c74b2478a5a5b304
Tofi <1812703@brunel.ac.uk>
2019-11-07 18:35:20 +0000

    Added a d-icebreaker function (Part 3a)

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -19,24 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void on_button_click(View view) {
 
-        TextView tv = this.findViewById(R.id.numberTextView) ;
+        roll_the_Dice();
+
+        TextView tv = this.findViewById(R.id.numberTextView);
         TextView en = (TextView) this.findViewById(R.id.EnterNumber);
         TextView si = (TextView) this.findViewById(R.id.ScoreId);
-
-
-        Random r = new Random();
-        int number = r.nextInt (6);
-
-
-        tv.setText(Integer.toString(number));
 
 
         int Number = Integer.valueOf(tv.getText().toString());
         int UserNumber = Integer.valueOf(en.getText().toString());
 
         if (UserNumber == Number) {
-          int Score = Integer.valueOf(si.getText().toString());
-          int ScoreChecker = Score + 1;
+            int Score = Integer.valueOf(si.getText().toString());
+            int ScoreChecker = Score + 1;
 
             si.setText(Integer.toString(ScoreChecker));
 
@@ -44,11 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+    }
+
+    public void on_button2_click(View view) {
+
+        roll_the_Dice();
+
+        TextView gb = this.findViewById(R.id.Question_box);
+        TextView tv = this.findViewById(R.id.numberTextView);
+
+        int number = Integer.valueOf(tv.getText().toString());
+
+        if (number == 1){
+            gb.setText("If you could go anywhere in the world, where would you go?");
+        }
+        else if (number == 2) {
+            gb.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+        }
+        else if (number == 3) {
+            gb.setText("If you could eat only one food for the rest of your life, what would that be?");
+        }
+        else if (number == 4) {
+            gb.setText("If you won a million dollars, what is the first thing you would buy?");
+        }
+        else if (number == 5) {
+            gb.setText("If you could spend the day with one fictional character, who would it be?");
+        }
+        else if (number == 6) {
+            gb.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+        }
+    }
 
 
+    public void roll_the_Dice(){
+
+        TextView tv = this.findViewById(R.id.numberTextView);
+
+        Random r = new Random();
+        int number = r.nextInt(6);
 
 
-
+        tv.setText(Integer.toString(number));
 
     }
 }
+
+
---
+++ app/src/main/java/com/example/diceroller/RandomQuestions.java
@@ -0,0 +1,16 @@
+package com.example.diceroller;
+
+public class RandomQuestions {
+        private String Questions[] = {
+                "If you could go anywhere in the world, where would you go?",
+                "If you were stranded on a desert island, what three things would you want to take with you?",
+                "If you could eat only one food for the rest of your life, what would that be?",
+                "If you won a million dollars, what is the first thing you would buy?",
+                "If you could spend the day with one fictional character, who would it be?",
+                "If you found a magic lantern and a genie gave you three wishes, what would you wish?"
+
+        };
+
+
+}
+
